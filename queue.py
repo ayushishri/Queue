c21adc45e4c7f83d86cbf2c2ebca4632f5a48d0c
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-06 21:27:01 -0400

    queue

--- queue.py
+++ queue.py
@@ -1,11 +1,21 @@
 class queue:
 	def __init__(self):
 		self.store=[]
+		self.count=0
+
 	def enQ(self,val):
 		self.store+=[val]
+		self.count+=1
 		return 0
+
 	def deQ(self):
 		rval=self.store[0]
 		self.store=self.store[1:]
-		return rval
+		self.count-=1
+		return [True,rval]
+		
+	def isEmpty(self):
+		if (self.count==0):
+			return True
+		return False
 
