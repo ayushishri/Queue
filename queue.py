6f8e60adfb528f64efd6416b29bac9ab329938f4
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-06 21:12:59 -0400

    queue

--- queue.py
+++ queue.py
@@ -1,1 +1,11 @@
+class queue:
+	def __init__(self):
+		self.store=[]
+	def enQ(self,val):
+		self.store+=[val]
+		return 0
+	def deQ(self):
+		rval=self.store[0]
+		self.store=self.store[1:]
+		return rval
 
